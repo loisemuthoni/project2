cb8a8998dd9130d4e4665ced187e96545b6c53a1
loisemuthoni <loisemburu01@gmail.com>
2019-10-28 15:54:53 +0300

    added a BDD at my README.md

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
 var submit = document.getElementById("submit");
 
-var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+submit.addEventListener("click", function() {
+    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var dd = document.getElementById("day").value;
-var mm = document.getElementById("month").value;
-var yy = document.getElementById("year").value;
-var date = new Date(yy + "-" + mm + "-" + dd);
-var daysOfWeek = date.getDay();
-var male = document.getElementsByName("male");
-var female = document.getElementsByName("female");
-var output = document.getElementById("output");
+    var dd = document.getElementById("day").value;
+    var mm = document.getElementById("month").value;
+    var yy = document.getElementById("year").value;
+    var date = new Date(yy + "-" + mm + "-" + dd);
+    var daysOfWeek = date.getDay();
+    var male = document.getElementsByName("male");
+    var female = document.getElementsByName("female");
+    var output = document.getElementById("output");
+    var name = document.getElementById("name");
 
-if (dd < 1 || dd > 31) {
-    alert("invalid day");
+    if (dd < 1 || dd > 31) {
+        alert("invalid day");
+    } else if (mm < 1 || mm > 12) {
+        alert("invalid month");
 
-} else if (mm < 1 || mm > 12) {
-    alert("invalid month");
-
-} else if (yy.toString().length !== 4) {
-    alert("invalid year");
-} else if (male.checked) {
-    output.innerHTML("Hey! Your Akan Name Is " + maleAkanNames[daysOfWeek])
-} else {
-    output.innerHTML("Hey! Your Akan Name Is " + femaleAkanNames[daysOfWeek])
-}
+    } else if (yy.toString().length !== 4) {
+        alert("invalid year");
+    } else if (male.checked) {
+        output.innerHTML = "Hey name! Your Akan Name Is " + maleAkanNames[daysOfWeek];
+    } else {
+        output.innerHTML = "Hey name! Your Akan Name Is " + femaleAkanNames[daysOfWeek];
+    }
+})
