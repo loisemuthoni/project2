5730ab9a8f1441bc91f7bc73080b328af434a4cf
loisemuthoni <loisemburu01@gmail.com>
2019-10-28 11:44:07 +0300

    correct submit & var

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var submit = document.getElementById("submit");
+
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -5,12 +7,11 @@ var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwam
 var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 function getDayOfWeek()
-var dd = parseInt(document.getElementById("day").value);
-var mm = parseInt(document.getElementById("month").value);
-var cc = parseInt((document.getElementById("year").value).slice(0, 2));
-var yy = parseInt((document.getElementById("year").value).slice(-2));
-var malegender = (document.getElementsByName("male").value);
-var femalegender = (document.getElementsByName("female").value);
-var submit = document.getElementById("submit");
+var dd = document.getElementById("day").value;
+var mm = document.getElementById("month").value;
+var cc = document.getElementById("year").value
+var yy = document.getElementById("year").value
+var malegender = document.getElementsByName("male");
+var femalegender = document.getElementsByName("female");
 
-var result = math.trunc(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
